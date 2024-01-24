b8fe0f94720b9a46bbccc6410dba21ac19b8bc80
Michael <michaelraffinpaculba@gmail.com>
2024-01-24 22:16:29 +0800

    add Clarity

--- pages/_app.js
+++ pages/_app.js
@@ -59,14 +59,14 @@ function MyApp({ Component, pageProps }) {
 </Script>
 
 
-<Script >
+{/* <Script >
  {`(function(c,l,a,r,i,t,y){
   c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
   t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
   y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
 })(window, document, "clarity", "script", "g7yfoivuno");
  `}
-</Script>
+</Script> */}
 
 
 
