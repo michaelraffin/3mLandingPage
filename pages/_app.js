b8ba9e89207d7e25e3280975f90affa8a8503dfd
Michael <michaelraffinpaculba@gmail.com>
2024-01-25 14:16:02 +0800

    Fix image display

--- pages/_app.js
+++ pages/_app.js
@@ -40,23 +40,14 @@ function MyApp({ Component, pageProps }) {
       strategy="afterInteractive"
       src="https://www.googletagmanager.com/gtag/js?id=UA-226511448-1"/>
 
-
-{/* <Script id="google-analytics">
- {`window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-5VVC6QK17C');
- `}
-</Script> */}
-
-<Script >
-{` (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "kqw0qreuki");
-    `}
-</Script>
+    <Script >
+    {` (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "kqw0qreuki");
+        `}
+    </Script>
 
 
 <Script id="google-analytics">
