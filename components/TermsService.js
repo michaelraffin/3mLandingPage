e5429389c99b5cb76c0712822c0d7cd96aed954b
Michael <michaelraffinpaculba@gmail.com>
2024-07-09 12:52:11 +0800

    replace Terms as 3mVisual

--- components/TermsService.js
+++ components/TermsService.js
@@ -1,71 +1,137 @@
-import React from 'react';
+import React from "react";
 
 function PolicyContent(props) {
-    return (
-        <div>
+  return (
+    <div>
+      <div className="lg:ml-60 lg:mr-60 lg:mb-40 md:ml-60 md:mr-60 md:mb-40">
+        <section class="relative pb-24">
+          <div class="max-w-6xl mx-auto px-4 sm:px-6 text-center">
+            <div class="py-24 md:py-36">
+              <h1 class="mb-5 text-6xl font-bold text-white">
+                Terms and Services
+              </h1>
+              <div class="space-y-4"></div>
+              <h1 class="mb-5 text-xs  lg:text-2xl text-lg md:text-2xl font-bold text-white">
+                Welcome to 3mVisual. These terms and services govern your use of
+                our website, products, and services. By accessing or using our
+                website, purchasing our products, or using our services, you
+                agree to be bound by these terms and services. If you do not
+                agree to these terms and services, please do not use our website
+                or services.
+              </h1>
+              <p class="text-md text-white"></p>
 
-<div className='lg:ml-60 lg:mr-60 lg:mb-40 md:ml-60 md:mr-60 md:mb-40'>
-                  <section class="relative pb-24">
-        <div class="max-w-6xl mx-auto px-4 sm:px-6 text-center">
-          <div class="py-24 md:py-36">
-            <h1 class="mb-5 text-6xl font-bold text-white">
-             Terms and Services
-            </h1>
-            <div class="space-y-4">
-                </div>
-                <h1 class="mb-5 text-xs  lg:text-2xl text-lg md:text-2xl font-bold text-white">
-                Welcome to SmartDeskPh. These terms and services govern your use of our website, products, and services. By accessing or using our website, purchasing our products, or using our services, you agree to be bound by these terms and services. If you do not agree to these terms and services, please do not use our website or services.
-                </h1>
+              <div className="t justify-items-start">
+                <h2 class="lg:text-2xl text-lg md:text-2xl font-bold text-white">
+                  Terms of Service
+                </h2>
                 <p class="text-md text-white">
-                
+                  By using our LED Wall and Lights n Sounds.("Product"), you
+                  agree to be bound by the following terms and conditions
+                  ("Terms of Service"):
                 </p>
-
-                <div className='t justify-items-start'>
-  <h2 class="lg:text-2xl text-lg md:text-2xl font-bold text-white">Terms of Service</h2>
-    <p class="text-md text-white">By using our LED Wall and Lights n Sounds.("Product"), you agree to be bound by the following terms and conditions ("Terms of Service"):</p>
-    <br/>
-  <h3 class="lg:text-2xl text-lg md:text-2xl  font-bold text-white">1. Use of the Product</h3>
-    <p class="text-md text-white">The rental period begins on [start date] and ends on [end date]</p>
-    <p class="text-md text-white">Any extension of the rental period must be agreed upon in writing and may be subject to additional fees.</p>
-    <br/>
- <h3 class="lg:text-2xl text-lg md:text-2xl  font-bold text-white">2. Payment</h3>
-    <p class="text-md text-white">A reservation is considered confirmed upon receipt of a signed rental agreement and a non-refundable deposit of [XX]% of the total rental cost.</p>
-    <p class="text-md text-white">The remaining balance must be paid [X] days prior to the scheduled rental date.</p>
-    <p class="text-md text-white">Payments can be made via [accepted payment methods].
-</p>
-
-<br/>
-  <h3 class="lg:text-2xl text-lg md:text-2xl  font-bold text-white">3. Delivery and Setup</h3>
-    <p class="text-md text-white">The client is responsible for providing a suitable and secure location for the LED wall setup. The client agrees to provide access to the venue and a designated contact person for coordination.</p>
-  l 
-
-  <h3 class="lg:text-2xl text-lg md:text-2xl  font-bold text-white">4. Termination</h3>
-    <p class="text-md text-white">The rental company reserves the right to terminate the rental agreement if the client breaches any terms outlined herein</p>
-  <br/>
-
-  
-  
-  <h3 class="lg:text-2xl text-lg md:text-2xl  font-bold text-white">5. Miscellaneous</h3>
-    <p class="text-md text-white">Any modifications to this agreement must be made in writing and agreed upon by both parties,</p>
-    <p class="text-md text-white">Failure to enforce any provision of this agreement does not waive the right to enforce such provision in the future.</p>
-  <br/>
-  <h3 class="lg:text-2xl text-lg md:text-2xl  font-bold text-white">6. Limitation of Liability</h3>
-    <p class="text-md text-white">We are not liable for any damages or losses arising from the use of the Product, including but not limited to direct, indirect, incidental, or consequential damages. Our liability is limited to the purchase price of the Product.</p>
- l <h3 class="lg:text-2xl text-lg md:text-2xl  font-bold text-white">7. Governing Law</h3>
-    <p class="text-md text-white">These Terms of Service shall be governed by and construed in accordance with the laws of the state where our business is located.</p>
- l <h3 class="lg:text-2xl text-lg md:text-2xl  font-bold text-white">8. Changes to the Terms of Service</h3>
-    <p class="text-md text-white">We reserve the right to modify these Terms of Service at any time. We will notify you of any changes by posting the updated Terms of Service on our website.</p>
- l <h3 class="lg:text-2xl text-lg md:text-2xl  font-bold text-white">9. Contact Us</h3>
-    <p class="text-md text-white">If you have any questions about these Terms of Service, please contact us at sales@3mvisual.com</p>
-</div>
-                </div>
-                </div>
-                {/* </div> */}
-                
-                </section>
-                </div>
-        </div>
-    );
+                <br />
+                <h3 class="lg:text-2xl text-lg md:text-2xl  font-bold text-white">
+                  1. Use of the Product
+                </h3>
+                <p class="text-md text-white">
+                  The rental period begins on [start date] and ends on [end
+                  date]
+                </p>
+                <p class="text-md text-white">
+                  Any extension of the rental period must be agreed upon in
+                  writing and may be subject to additional fees.
+                </p>
+                <br />
+                <h3 class="lg:text-2xl text-lg md:text-2xl  font-bold text-white">
+                  2. Payment
+                </h3>
+                <p class="text-md text-white">
+                  A reservation is considered confirmed upon receipt of a signed
+                  rental agreement and a non-refundable deposit of [XX]% of the
+                  total rental cost.
+                </p>
+                <p class="text-md text-white">
+                  The remaining balance must be paid [X] days prior to the
+                  scheduled rental date.
+                </p>
+                <p class="text-md text-white">
+                  Payments can be made via [accepted payment methods].
+                </p>
+                <br />
+                <h3 class="lg:text-2xl text-lg md:text-2xl  font-bold text-white">
+                  3. Delivery and Setup
+                </h3>
+                <p class="text-md text-white">
+                  The client is responsible for providing a suitable and secure
+                  location for the LED wall setup. The client agrees to provide
+                  access to the venue and a designated contact person for
+                  coordination.
+                </p>
+                l
+                <h3 class="lg:text-2xl text-lg md:text-2xl  font-bold text-white">
+                  4. Termination
+                </h3>
+                <p class="text-md text-white">
+                  The rental company reserves the right to terminate the rental
+                  agreement if the client breaches any terms outlined herein
+                </p>
+                <br />
+                <h3 class="lg:text-2xl text-lg md:text-2xl  font-bold text-white">
+                  5. Miscellaneous
+                </h3>
+                <p class="text-md text-white">
+                  Any modifications to this agreement must be made in writing
+                  and agreed upon by both parties,
+                </p>
+                <p class="text-md text-white">
+                  Failure to enforce any provision of this agreement does not
+                  waive the right to enforce such provision in the future.
+                </p>
+                <br />
+                <h3 class="lg:text-2xl text-lg md:text-2xl  font-bold text-white">
+                  6. Limitation of Liability
+                </h3>
+                <p class="text-md text-white">
+                  We are not liable for any damages or losses arising from the
+                  use of the Product, including but not limited to direct,
+                  indirect, incidental, or consequential damages. Our liability
+                  is limited to the purchase price of the Product.
+                </p>
+                l{" "}
+                <h3 class="lg:text-2xl text-lg md:text-2xl  font-bold text-white">
+                  7. Governing Law
+                </h3>
+                <p class="text-md text-white">
+                  These Terms of Service shall be governed by and construed in
+                  accordance with the laws of the state where our business is
+                  located.
+                </p>
+                l{" "}
+                <h3 class="lg:text-2xl text-lg md:text-2xl  font-bold text-white">
+                  8. Changes to the Terms of Service
+                </h3>
+                <p class="text-md text-white">
+                  We reserve the right to modify these Terms of Service at any
+                  time. We will notify you of any changes by posting the updated
+                  Terms of Service on our website.
+                </p>
+                l{" "}
+                <h3 class="lg:text-2xl text-lg md:text-2xl  font-bold text-white">
+                  9. Contact Us
+                </h3>
+                <p class="text-md text-white">
+                  If you have any questions about these Terms of Service, please
+                  contact us at sales@3mvisual.com
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* </div> */}
+        </section>
+      </div>
+    </div>
+  );
 }
 
 export default PolicyContent;
